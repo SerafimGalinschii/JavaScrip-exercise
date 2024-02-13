54332e7bb2d8cd87042bd841d3220ac1ff663146
serafim.galinschii@gmail.com <serafim.galinschii@gmail.com>
2024-02-13 21:26:55 +0200

    code debuging

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -33,21 +33,7 @@ function displayOnKey(event) {
   } else {
     return "Error";
   }
-  addToArray();
+  addTask();
 }
 
-const toDoList = ["english", "sofware", "selfdevelop"];
 
-for (index = 0; index < toDoList.length; index++) {
-  const value = toDoList[index];
-  console.log(value);
-}
-
-const array = [1, 2, 3];
-let total = 0;
-
-for (index = 0; index < array.length; index++) {
-  num = array[index];
-  total += num;
-}
-console.log(total);
